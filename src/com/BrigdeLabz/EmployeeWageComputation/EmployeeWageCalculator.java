b4e3fcf3e0f0b3ffdaff39aef58771b7d1812cd8
Swathi <swathi.h@perfios.com>
2021-08-20 12:00:51 +0530

    Calculating wages for a month

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++ src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
@@ -1,26 +1,36 @@
 package com.BrigdeLabz.EmployeeWageComputation;
 
 public class EmployeeWageCalculator {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMPLOYEE_RATE_PER_HOUR = 20;
+	public static final int NO_OF_WORKING_DAYS = 20;
+
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Employee Wage Computation *****");
-		int isPartTime = 1;
-		int isFullTime = 2;
-		int employeeRatePerHour = 20;
 		int employeeHours = 0;
 		int employeeWage = 0;
-		double employeeCheck = Math.floor(Math.random() * 10) % 3;
+		int totalEmployeeWage = 0;
+		for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
+			int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (employeeCheck) {
+			case IS_FULL_TIME:
+				employeeHours = 8;
+				break;
 
-		if (employeeCheck == isFullTime) {
-			employeeHours = 8;
-			
-		} else if (employeeCheck == isPartTime) {
-			employeeHours = 4;
-			
-		} else {
-			employeeHours = 0;
+			case IS_PART_TIME:
+				employeeHours = 4;
+				break;
+
+			case 0:
+				employeeHours = 0;
+				break;
+			}
+
+			employeeWage = employeeHours * EMPLOYEE_RATE_PER_HOUR;
+			totalEmployeeWage += employeeWage;
+			System.out.println("Employee wage: " + employeeWage);
 		}
-
-		employeeWage = employeeHours * employeeRatePerHour;
-		System.out.println("Employee wage: " + employeeWage);
+		System.out.println("Total Employee wage: " + totalEmployeeWage);
 	}
 }
