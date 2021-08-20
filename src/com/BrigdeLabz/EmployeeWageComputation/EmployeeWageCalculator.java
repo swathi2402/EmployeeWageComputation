cd8915254e9f96858caf8a7466dc2b7b4b805484
Swathi <swathi.h@perfios.com>
2021-08-20 12:23:44 +0530

    Conditioned wage calculation

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++ src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
@@ -5,13 +5,16 @@ public class EmployeeWageCalculator {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMPLOYEE_RATE_PER_HOUR = 20;
 	public static final int NO_OF_WORKING_DAYS = 20;
+	public static final int MAX_HOURS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Employee Wage Computation *****");
 		int employeeHours = 0;
-		int employeeWage = 0;
 		int totalEmployeeWage = 0;
-		for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
+		int totalEmployeeHours = 0;
+		int totalWorkingDays = 0;
+		while (totalEmployeeHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (employeeCheck) {
 			case IS_FULL_TIME:
@@ -26,11 +29,10 @@ public class EmployeeWageCalculator {
 				employeeHours = 0;
 				break;
 			}
-
-			employeeWage = employeeHours * EMPLOYEE_RATE_PER_HOUR;
-			totalEmployeeWage += employeeWage;
-			System.out.println("Employee wage: " + employeeWage);
+			totalEmployeeHours += employeeHours;
+			System.out.println("day: " + totalWorkingDays + " Employee Hour: " + employeeHours);
 		}
+		totalEmployeeWage = totalEmployeeHours * EMPLOYEE_RATE_PER_HOUR;
 		System.out.println("Total Employee wage: " + totalEmployeeWage);
 	}
 }
