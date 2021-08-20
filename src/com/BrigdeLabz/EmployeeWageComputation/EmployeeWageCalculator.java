6003c51ae8447c106ea8f71b1fbfa71a39269ff9
Swathi <swathi.h@perfios.com>
2021-08-20 11:01:50 +0530

    Revert "Initial commit"
    This reverts commit 1d4c2b2259181c65c51f785d5c4e4074b03a6c37.

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++
@@ -1,7 +0,0 @@
-package com.BrigdeLabz.EmployeeWageComputation;
-
-public class EmployeeWageCalculator {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
-	}
-}
