f78536c3aeb7c42bfd3bbbf82793dc87ad12b2c7
Swathi <swathi.h@perfios.com>
2021-08-20 11:38:36 +0530

    Emplyoee Check

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++ src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
@@ -2,6 +2,13 @@ package com.BrigdeLabz.EmployeeWageComputation;
 
 public class EmployeeWageCalculator {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
+		System.out.println("***** Welcome to Employee Wage Computation *****");
+		int employeePresent = 1;
+		double isEmployeePresent = Math.floor(Math.random() * 10) % 2;
+		if (isEmployeePresent == employeePresent) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
