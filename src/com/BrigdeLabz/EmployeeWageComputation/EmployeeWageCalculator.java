1545273474c96c11e7acfeaf600079ee36b404bd
Swathi <swathi.h@perfios.com>
2021-08-20 11:49:17 +0530

    Adding Part time employee and wage

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++ src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
@@ -3,18 +3,23 @@ package com.BrigdeLabz.EmployeeWageComputation;
 public class EmployeeWageCalculator {
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Employee Wage Computation *****");
-		int isFullTime = 1;
+		int isPartTime = 1;
+		int isFullTime = 2;
 		int employeeRatePerHour = 20;
 		int employeeHours = 0;
 		int employeeWage = 0;
-		double employeeCheck = Math.floor(Math.random() * 10) % 2;
-		
-		if(employeeCheck == isFullTime) {
+		double employeeCheck = Math.floor(Math.random() * 10) % 3;
+
+		if (employeeCheck == isFullTime) {
 			employeeHours = 8;
+			
+		} else if (employeeCheck == isPartTime) {
+			employeeHours = 4;
+			
 		} else {
 			employeeHours = 0;
 		}
-		
+
 		employeeWage = employeeHours * employeeRatePerHour;
 		System.out.println("Employee wage: " + employeeWage);
 	}
