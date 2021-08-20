3accc4609a67ff28a1e4036b4cdff776c22a39ba
Swathi <swathi.h@perfios.com>
2021-08-20 11:44:33 +0530

    Daily Employee Wage

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++ src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
@@ -3,12 +3,19 @@ package com.BrigdeLabz.EmployeeWageComputation;
 public class EmployeeWageCalculator {
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Employee Wage Computation *****");
-		int employeePresent = 1;
-		double isEmployeePresent = Math.floor(Math.random() * 10) % 2;
-		if (isEmployeePresent == employeePresent) {
-			System.out.println("Employee is Present");
+		int isFullTime = 1;
+		int employeeRatePerHour = 20;
+		int employeeHours = 0;
+		int employeeWage = 0;
+		double employeeCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if(employeeCheck == isFullTime) {
+			employeeHours = 8;
 		} else {
-			System.out.println("Employee is Absent");
+			employeeHours = 0;
 		}
+		
+		employeeWage = employeeHours * employeeRatePerHour;
+		System.out.println("Employee wage: " + employeeWage);
 	}
 }
