26d6b934974d2682c8fdd26fcb70a9853fa99c56
Swathi <swathi.h@perfios.com>
2021-08-26 17:21:21 +0530

    ADDED: Logic to calculate employee daily wage

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++ src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
@@ -1,15 +1,22 @@
 package com.BrigdeLabz.EmployeeWageComputation;
 
 public class EmployeeWageCalculator {
+	public static final int EMPLOYEE_RATE_PER_HOUR = 20;
+	
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Employee Wage Computation *****");
 		int isPresent = 1;
-		double employeeCheck = Math.floor(Math.random() * 10) % 2;
+		int employeeHours = 0;
+		int employeeWage = 0;
+		int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
 		if (employeeCheck == isPresent) {
-			System.out.println("Employee is Present");
+			employeeHours = 8;
 		}
 		else {
-			System.out.println("Employee is Absent");
+			employeeHours = 0;
 		}
+		employeeWage = employeeHours * EMPLOYEE_RATE_PER_HOUR;
+		System.out.println("Employee wage: " + employeeWage);
+
 	}
 }
