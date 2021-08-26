100e321de2d58d069f8b2ff89f3a46521be547d0
Swathi <swathi.h@perfios.com>
2021-08-26 18:54:20 +0530

    ADDED: Method to calculate employee wage for many companies

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++ src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
@@ -3,16 +3,25 @@ package com.BrigdeLabz.EmployeeWageComputation;
 public class EmployeeWageCalculator {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMPLOYEE_RATE_PER_HOUR = 20;
-	public static final int NO_OF_WORKING_DAYS = 20;
-	public static final int MAX_HOURS_IN_MONTH = 100;
 	
-	public static void employeeWage() {
+	private String companyName;
+	private int employeeRatePerHour;
+	private int numOfWorkingDays;
+	private int maxHoursInMonth;
+	
+	private EmployeeWageCalculator(String companyName, int employeeRatePerHour, int numOfWorkingDays,
+			int maxHoursInMonth) {
+		super();
+		this.companyName = companyName;
+		this.employeeRatePerHour = employeeRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursInMonth = maxHoursInMonth;
+	
 		int employeeHours = 0;
 		int totalEmployeeWage = 0;
 		int totalEmployeeHours = 0;
 		int totalWorkingDays = 0;
-		while (totalEmployeeHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
+		while (totalEmployeeHours <= maxHoursInMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 			int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (employeeCheck) {
@@ -29,14 +38,16 @@ public class EmployeeWageCalculator {
 				break;
 			}
 			totalEmployeeHours += employeeHours;
-			System.out.println("day: " + totalWorkingDays + " Employee Wage: " + employeeHours * EMPLOYEE_RATE_PER_HOUR);
+			System.out.println("day: " + totalWorkingDays + " Employee Wage: " + employeeHours * employeeRatePerHour);
 		}
-		totalEmployeeWage = totalEmployeeHours * EMPLOYEE_RATE_PER_HOUR;
-		System.out.println("Total Employee wage: " + totalEmployeeWage);
+		totalEmployeeWage = totalEmployeeHours * employeeRatePerHour;
+		System.out.println("Total Employee wage for " +companyName + " is: " + totalEmployeeWage);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Employee Wage Computation *****");
-		employeeWage();
+		new EmployeeWageCalculator("DMart", 20, 20, 100);
+		new EmployeeWageCalculator("Relaince", 30, 25, 150);
+		
 	}
 }
