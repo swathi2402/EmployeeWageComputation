12cb7e437e53f41217dc3e434a7efb082f378a77
Swathi <swathi.h@perfios.com>
2021-08-26 17:27:18 +0530

    ADDED: Part time employee added through if statement

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++ src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
@@ -1,16 +1,20 @@
 package com.BrigdeLabz.EmployeeWageComputation;
 
 public class EmployeeWageCalculator {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
 	public static final int EMPLOYEE_RATE_PER_HOUR = 20;
 	
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Employee Wage Computation *****");
-		int isPresent = 1;
 		int employeeHours = 0;
 		int employeeWage = 0;
 		int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
-		if (employeeCheck == isPresent) {
+		if (employeeCheck == IS_FULL_TIME) {
 			employeeHours = 8;
+		}
+		else if(employeeCheck == IS_PART_TIME) {
+			employeeHours = 4;
 		}
 		else {
 			employeeHours = 0;
