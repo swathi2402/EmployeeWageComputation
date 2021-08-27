8f34e91e0381386abd824dfd565ff53fdbf36bd1
Swathi <swathi.h@perfios.com>
2021-08-27 16:48:12 +0530

    REFACTORED: Changed to get input with function parameters

--- src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
+++ src/com/BrigdeLabz/EmployeeWageComputation/EmployeeWageCalculator.java
@@ -3,20 +3,10 @@ package com.BrigdeLabz.EmployeeWageComputation;
 public class EmployeeWageCalculator {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	
-	private String companyName;
-	private int employeeRatePerHour;
-	private int numOfWorkingDays;
-	private int maxHoursInMonth;
-	
-	private EmployeeWageCalculator(String companyName, int employeeRatePerHour, int numOfWorkingDays,
+
+	public static void ComputeEmployeeWage(String companyName, int employeeRatePerHour, int numOfWorkingDays,
 			int maxHoursInMonth) {
-		super();
-		this.companyName = companyName;
-		this.employeeRatePerHour = employeeRatePerHour;
-		this.numOfWorkingDays = numOfWorkingDays;
-		this.maxHoursInMonth = maxHoursInMonth;
-	
+
 		int employeeHours = 0;
 		int totalEmployeeWage = 0;
 		int totalEmployeeHours = 0;
@@ -41,13 +31,13 @@ public class EmployeeWageCalculator {
 			System.out.println("day: " + totalWorkingDays + " Employee Wage: " + employeeHours * employeeRatePerHour);
 		}
 		totalEmployeeWage = totalEmployeeHours * employeeRatePerHour;
-		System.out.println("Total Employee wage for " +companyName + " is: " + totalEmployeeWage);
+		System.out.println("Total Employee wage for " + companyName + " is: " + totalEmployeeWage);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Employee Wage Computation *****");
-		new EmployeeWageCalculator("DMart", 20, 20, 100);
-		new EmployeeWageCalculator("Relaince", 30, 25, 150);
-		
+		ComputeEmployeeWage("DMart", 20, 20, 100);
+		ComputeEmployeeWage("Relaince", 30, 25, 150);
+
 	}
 }
